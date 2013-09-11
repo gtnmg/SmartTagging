50884e43870f961d7426ee444ca94e2bb47edc6e
Mikhail Litvin <mozg@mozg-book.(none)>
2013-09-11 22:23:59 +0200

    modified terms activity, added buttons for select catalog

--- src/great/team/activities/TermsOverviewActivity.java
+++ src/great/team/activities/TermsOverviewActivity.java
@@ -3,30 +3,36 @@ package great.team.activities;
 import great.team.R;
 import great.team.db.DataProviderFactory;
 import great.team.db.IDataProvider;
+import great.team.dialogs.SelectCatalogDialog;
 import great.team.entity.Catalog;
 import great.team.entity.Term;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class TermsOverviewActivity extends Activity {
+public class TermsOverviewActivity extends Activity implements OnClickListener{
 
 	ListView mTermsListView;
-	Catalog  mCurrentCatalog = null;
+	Button mBtnSelectCatalog;
+	TextView mSelectedCatalog;
+	Catalog mCurrentCatalog = null;
+	
 
 	private String[] getTerms() {
 		IDataProvider dataProvider = DataProviderFactory
 				.getDataProvider(getApplicationContext());
 		return dataProvider.getTerms(mCurrentCatalog);
 	}
-	private void init()
-	{
+
+	private void init() {
 		mTermsListView = (ListView) findViewById(R.id.terms_list_view);
 
 		mTermsListView.setAdapter(new ArrayAdapter<String>(this,
@@ -43,12 +49,24 @@ public class TermsOverviewActivity extends Activity {
 				startActivity(i);
 			}
 		});
-
+		mSelectedCatalog = (TextView)findViewById(R.id.tvSelectedCatalog);
+		mBtnSelectCatalog = (Button) findViewById(R.id.btnCatalogSelect);
+		mBtnSelectCatalog.setOnClickListener(this);
 	}
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_terms_overview);
 		init();
+	}
+
+	@Override
+	public void onClick(View v) {
+		if(v==mBtnSelectCatalog)
+		{
+			mCurrentCatalog = SelectCatalogDialog.execute(this);
+			mSelectedCatalog.setText(mCurrentCatalog==null?"":mCurrentCatalog.getName());
+		}
 	}
 
 }
